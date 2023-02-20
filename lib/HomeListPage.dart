8fc35c7c34439b9c1b23fbc0380c48276525a849
mpradipfunds <pradeep@fundsindia.com>
2023-02-20 14:27:54 +0530

    Add font

--- lib/HomeListPage.dart
+++ lib/HomeListPage.dart
@@ -29,32 +29,45 @@ class HomeListPageState extends State<HomeListPage> {
               return Scaffold(
                 body: CustomScrollView(
                   slivers: <Widget>[
-                    SliverPersistentHeader(
-                      delegate: SectionHeaderDelegate(beers[0]['title']),
-                      pinned: true,
+                    SliverAppBar(
+                      backgroundColor: Colors.transparent,
+                      pinned: false,
+                      snap: false,
+                      floating: true,
+                      flexibleSpace: FlexibleSpaceBar(
+                        title: Text(beers[0]['title'],style: TextStyle(
+                          fontFamily: 'Poppins',
+                          fontWeight: FontWeight.w500,
+                          color: Color(0xff000000),
+                          fontSize: 18.0,
+                        ),),
+                      ),
                     ),
                     SliverToBoxAdapter(
                       child: Container(
                         height: 200.0,
+                        width: 200.0,
                         child: ListView.builder(
                           scrollDirection: Axis.horizontal,
                           itemCount:
-                          beers == null ? 0 : beers[0]['items'].length,
+                              beers == null ? 0 : beers[0]['items'].length,
                           itemBuilder: (context, index) {
                             var beer = beers[0];
                             var id = beer['items'][index]['id'] as String;
                             var title = beer['items'][index]['title'] as String;
                             var image = beer['items'][index]['image'] as String;
                             var description =
-                            beer['items'][index]['description'] as String;
+                                beer['items'][index]['description'] as String;
                             var price = beer['items'][index]['price'] as String;
                             var offer_price =
-                            beer['items'][index]['offer_price'] as String;
+                                beer['items'][index]['offer_price'] as String;
                             var productlabel =
-                            beer['items'][index]['productlabel'] as String;
+                                beer['items'][index]['productlabel'] as String;
+                            if (productlabel.isEmpty) {
+                              productlabel = "0%";
+                            }
                             return Container(
-                              width: 100,
-                              margin: EdgeInsets.all(3),
+                              width: 120,
                               height: 900,
                               color: Colors.transparent,
                               child: Column(
@@ -72,14 +85,15 @@ class HomeListPageState extends State<HomeListPage> {
                                           ),
                                           footer: Container(
                                             child: Padding(
-                                              padding: const EdgeInsets.all(8.0),
+                                              padding:
+                                                  const EdgeInsets.all(8.0),
                                               child: Text(
                                                 textAlign: TextAlign.end,
-                                                price+"%",
+                                                productlabel,
                                                 style: TextStyle(
                                                   backgroundColor: Colors.white,
                                                   fontWeight: FontWeight.bold,
-                                                  color: Colors.red,
+                                                  color: Color(0xffEC0400),
                                                   fontSize: 10,
                                                 ),
                                               ),
@@ -89,44 +103,61 @@ class HomeListPageState extends State<HomeListPage> {
                                       ),
                                     ),
                                   ),
-                                  Expanded(flex: 1, child: Text(title)),
-                                  Expanded(flex: 1, child: Text(description)),
-                                  Expanded(flex: 1, child: Text(price)),
-
-                                ],
-                              ),
-                              /*     child: ClipRRect(
-                                    borderRadius: BorderRadius.circular(12),
-                                    child: InkWell(
-                                      onTap: () {},
-                                      child: GridTile(
-                                        child: Image.network(
-                                          image,
-                                          fit: BoxFit.fill,
-                                        ),
-                                        header: CircleAvatar(
-                                          backgroundImage: AssetImage(
-                                              "assets/heart.png"),
-                                          radius: 200,
-                                        ),
-                                        footer: Container(
-                                          child: Padding(
-                                            padding: const EdgeInsets.all(8.0),
-                                            child: Text(
-                                              textAlign: TextAlign.end,
-                                             price+"%",
-                                              style: TextStyle(
-                                                backgroundColor: Colors.white,
-                                                fontWeight: FontWeight.bold,
-                                                color: Colors.red,
-                                                fontSize: 10,
-                                              ),
+                                  Expanded(
+                                    flex: 1,
+                                    child: Text(
+                                      title,
+                                      textAlign: TextAlign.start, style: new TextStyle(
+                                      fontFamily: 'Poppins',
+                                      fontWeight: FontWeight.w400,
+                                      fontSize: 14.0,
+                                      color: Color(0xff000000),
+                                    ),
+                                    ),
+                                  ),
+                                  Expanded(
+                                    flex: 2,
+                                    child: Text(
+                                      description,
+                                      textAlign: TextAlign.start,  style: new TextStyle(
+                                      fontFamily: 'Poppins',
+                                      fontWeight: FontWeight.w400,
+                                      fontSize: 10.0,
+                                      color: Color(0xffA9A9A9),
+                                    ),
+                                    ),
+                                  ),
+                                  Expanded(
+                                    flex: 1,
+                                    child: Text.rich(
+                                      TextSpan(
+                                        text: '',
+                                        children: <TextSpan>[
+                                          new TextSpan(
+                                            text: price,
+                                            style: new TextStyle(
+                                              fontFamily: 'Poppins',
+                                              fontWeight: FontWeight.w400,
+                                              fontSize: 10.0,
+                                              color: Color(0xffA9A9A9),
+                                              decoration:
+                                                  TextDecoration.lineThrough,
                                             ),
                                           ),
-                                        ),
+                                          new TextSpan(
+                                            text: ' '+offer_price,style: new TextStyle(
+                                            fontSize: 10.0,
+                                            fontFamily: 'Poppins',
+                                            fontWeight: FontWeight.w400,
+                                            color: Color(0xffD92E20),
+                                          ),
+                                          ),
+                                        ],
                                       ),
                                     ),
-                                  ),*/
+                                  ),
+                                ],
+                              ),
                             );
                           },
                         ),
@@ -139,6 +170,7 @@ class HomeListPageState extends State<HomeListPage> {
                     SliverToBoxAdapter(
                       child: Container(
                         height: 200.0,
+                        width: 200.0,
                         child: ListView.builder(
                           scrollDirection: Axis.horizontal,
                           itemCount:
@@ -155,10 +187,13 @@ class HomeListPageState extends State<HomeListPage> {
                                 beer['items'][index]['offer_price'] as String;
                             var productlabel =
                                 beer['items'][index]['productlabel'] as String;
+                            if (productlabel.isEmpty) {
+                              productlabel = "0%";
+                            }
                             return Container(
                               width: 100,
-                              margin: EdgeInsets.all(3),
                               height: 900,
+                              margin: EdgeInsets.fromLTRB(0, 0, 10, 0),
                               color: Colors.transparent,
                               child: Column(
                                 children: [
@@ -175,10 +210,11 @@ class HomeListPageState extends State<HomeListPage> {
                                           ),
                                           footer: Container(
                                             child: Padding(
-                                              padding: const EdgeInsets.all(8.0),
+                                              padding:
+                                                  const EdgeInsets.all(8.0),
                                               child: Text(
                                                 textAlign: TextAlign.end,
-                                                price+"%",
+                                                productlabel,
                                                 style: TextStyle(
                                                   backgroundColor: Colors.white,
                                                   fontWeight: FontWeight.bold,
@@ -192,44 +228,58 @@ class HomeListPageState extends State<HomeListPage> {
                                       ),
                                     ),
                                   ),
-                                  Expanded(flex: 1, child: Text(title)),
-                                  Expanded(flex: 1, child: Text(description)),
-                                  Expanded(flex: 1, child: Text(price)),
-
-                                ],
-                              ),
-                              /*     child: ClipRRect(
-                                    borderRadius: BorderRadius.circular(12),
-                                    child: InkWell(
-                                      onTap: () {},
-                                      child: GridTile(
-                                        child: Image.network(
-                                          image,
-                                          fit: BoxFit.fill,
-                                        ),
-                                        header: CircleAvatar(
-                                          backgroundImage: AssetImage(
-                                              "assets/heart.png"),
-                                          radius: 200,
-                                        ),
-                                        footer: Container(
-                                          child: Padding(
-                                            padding: const EdgeInsets.all(8.0),
-                                            child: Text(
-                                              textAlign: TextAlign.end,
-                                             price+"%",
-                                              style: TextStyle(
-                                                backgroundColor: Colors.white,
-                                                fontWeight: FontWeight.bold,
-                                                color: Colors.red,
-                                                fontSize: 10,
-                                              ),
+                                  Expanded(
+                                    flex: 1,
+                                    child: Text(
+                                      title,
+                                      textAlign: TextAlign.start,
+                                    ),
+                                  ),
+                                  Expanded(
+                                    flex: 1,
+                                    child: Text(
+                                      description,
+                                      textAlign: TextAlign.start,
+                                    ),
+                                  ),
+                                  Expanded(
+                                    flex: 2,
+                                    child: Text.rich(
+                                      TextSpan(
+                                        text: '',
+                                        children: <TextSpan>[
+                                          new TextSpan(
+                                            text: price,
+                                            style: new TextStyle(
+                                              fontFamily: 'Poppins',
+                                              fontWeight: FontWeight.w400,
+                                              fontSize: 10.0,
+                                              color: Color(0xffA9A9A9),
+                                              decoration:
+                                              TextDecoration.lineThrough,
                                             ),
                                           ),
-                                        ),
+                                          new TextSpan(
+                                            text: ' '+offer_price,style: new TextStyle(
+                                            fontSize: 10.0,
+                                            fontFamily: 'Poppins',
+                                            fontWeight: FontWeight.w400,
+                                            color: Color(0xffD92E20),
+                                          ),
+                                          ),
+                                        ],
                                       ),
                                     ),
+                                  ),
+                                /*  Expanded(
+                                    flex: 1,
+                                    child: Text(
+                                      price,
+                                      textAlign: TextAlign.start,
+                                    ),
                                   ),*/
+                                ],
+                              ),
                             );
                           },
                         ),
@@ -249,7 +299,8 @@ class HomeListPageState extends State<HomeListPage> {
                           itemBuilder: (context, index) {
                             var beer = beers[2];
                             var id = beer['items'][index]['id'] as int;
-                            var title = beer['items'][index]['__typename'] as String;
+                            var title =
+                                beer['items'][index]['__typename'] as String;
                             var name = beer['items'][index]['name'] as String;
 
                             var image = beer['items'][index]['small_image']
@@ -257,13 +308,15 @@ class HomeListPageState extends State<HomeListPage> {
                             var description = beer['items'][index]
                                 ['short_description']["html"] as String;
                             var regular_price_type = beer['items'][index]
-                            ['price_range']["maximum_price"]["regular_price"]["currency"] as String;
+                                    ['price_range']["maximum_price"]
+                                ["regular_price"]["currency"] as String;
                             var regular_price_value = beer['items'][index]
-                            ['price_range']["maximum_price"]["regular_price"]["value"] as int;
-                            var regular_price = regular_price_type+regular_price_value.toString();
+                                    ['price_range']["maximum_price"]
+                                ["regular_price"]["value"] as int;
+                            var regular_price = regular_price_type +
+                                regular_price_value.toString();
                             return Container(
                               width: 100,
-                              margin: EdgeInsets.all(3),
                               height: 900,
                               color: Colors.transparent,
                               child: Column(
@@ -281,7 +334,8 @@ class HomeListPageState extends State<HomeListPage> {
                                           ),
                                           footer: Container(
                                             child: Padding(
-                                              padding: const EdgeInsets.all(8.0),
+                                              padding:
+                                                  const EdgeInsets.all(8.0),
                                               child: Text(
                                                 textAlign: TextAlign.end,
                                                 regular_price,
